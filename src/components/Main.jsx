5a30a2f0ef09223fa5d653d9a8d1850a43db5bd8
LucioFurnari <luciofurnari99@gmail.com>
2023-04-08 18:22:01 -0300

    Create function to randomize the array

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,33 @@
+import Card from "./Card"
+import { useState } from 'react'
+
 export default function Main() {
+  const [cards, setCards] = useState([1,2,3,4,5,6,7,8,9])
+
+  function handleClick(ev) {
+    ev.stopPropagation()
+    const {id} = ev.currentTarget
+    console.log(id)
+    
+  }
+
+  function shuffle() {
+    let currentIndex = cards.length
+    let randomize;
+    let newArr = cards;
+    while (currentIndex != 0) {
+      randomize = Math.floor(Math.random() * currentIndex);
+      currentIndex = currentIndex - 1;
+      [newArr[currentIndex], newArr[randomize]] = [newArr[randomize],newArr[currentIndex]]
+    }
+    console.log(newArr)
+    setCards(newArr)
+  }
+  
   return (
-    <main>
-      
-    </main>
+    <div className="card-grid">
+      {cards.map((item,index) => <Card key={index} id={index} number={item} handleClick={shuffle}/>)
+      }
+    </div>
   )
 }
