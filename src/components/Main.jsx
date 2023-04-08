b07519883c4cd07b0a6dcced03099588c7ea2418
LucioFurnari <luciofurnari99@gmail.com>
2023-04-08 19:44:55 -0300

    Fix shuffle function, to change the cards state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,13 @@
 import Card from "./Card"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Main() {
   const [cards, setCards] = useState([1,2,3,4,5,6,7,8,9])
 
-  function handleClick(ev) {
-    ev.stopPropagation()
-    const {id} = ev.currentTarget
-    console.log(id)
-    
-  }
-
   function shuffle() {
     let currentIndex = cards.length
     let randomize;
-    let newArr = cards;
+    let newArr = cards.slice();
     while (currentIndex != 0) {
       randomize = Math.floor(Math.random() * currentIndex);
       currentIndex = currentIndex - 1;
@@ -26,7 +19,8 @@ export default function Main() {
   
   return (
     <div className="card-grid">
-      {cards.map((item,index) => <Card key={index} id={index} number={item} handleClick={shuffle}/>)
+      {
+      cards.map((item,index) => <Card key={item.toString()} id={index} number={item} handleClick={shuffle}/>)
       }
     </div>
   )
