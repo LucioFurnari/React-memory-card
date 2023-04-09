9b6cb758e2fd5d3d1931e4e996d055884b185cbd
LucioFurnari <luciofurnari99@gmail.com>
2023-04-09 19:09:48 -0300

    Remove state and functions

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,46 +1,7 @@
 import Card from "./Card"
-import { useEffect, useState } from 'react'
 
-export default function Main() {
-  const [cards, setCards] = useState([
-    {n:1, clicked: false},
-    {n:2, clicked: false},
-    {n:3, clicked: false},
-    {n:4, clicked: false},
-    {n:5, clicked: false},
-    {n:6, clicked: false},
-    {n:7, clicked: false},
-    {n:8, clicked: false},
-    {n:9, clicked: false},
-  ])
-
-  function selectCard(id) {
-    const newArr = cards.map((item, index) => {
-      if(index == id) {
-        item.clicked = true;
-      }
-      return item
-    })
-    setCards(newArr)
-  }
-
-  function shuffle(ev) {
-    ev.stopPropagation()
-    const { id } = ev.currentTarget
-    console.log(ev.currentTarget)
-    selectCard(id)
-    let currentIndex = cards.length
-    let randomize;
-    let newArr = cards.slice();
-    while (currentIndex != 0) {
-      randomize = Math.floor(Math.random() * currentIndex);
-      currentIndex = currentIndex - 1;
-      [newArr[currentIndex], newArr[randomize]] = [newArr[randomize],newArr[currentIndex]]
-    }
-    console.log(newArr)
-    setCards(newArr)
-  }
-  
+export default function Main(props) {
+  const { cards, shuffle } = props;
   return (
     <div className="card-grid">
       {
