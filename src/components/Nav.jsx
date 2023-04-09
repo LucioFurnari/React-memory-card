b471ddaf4364603551be1620000d2a7aa65bf666
LucioFurnari <luciofurnari99@gmail.com>
2023-04-09 19:10:44 -0300

    Add simple nav and score

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,13 @@
-export default function Nav() {
+export default function Nav(props) {
+  const { score, bestScore } = props;
   return (
     <nav>
-      
+      <h1>Memory Card</h1>
+      <p>Get points by clicking on an image but don't click on any more than once!</p>
+      <div className="score-table">
+        <span>Score: {score}</span>
+        <span>Best score: {bestScore}</span>
+      </div>
     </nav>
   )
 }
