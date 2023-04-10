7e23852e18ea83d85bfc2b204e351d6687a312b7
LucioFurnari <luciofurnari99@gmail.com>
2023-04-10 12:13:46 -0300

    Add src props to the Cards in Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,16 @@
 import Card from "./Card"
 
 export default function Main(props) {
-  const { cards, shuffle } = props;
+  const { cards, shuffle} = props;
   return (
     <div className="card-grid">
       {
-      cards.map((item,index) => <Card key={item.n} id={index} number={item.n} handleClick={shuffle}/>)
+      cards.map((item,index) => <Card 
+      key={item.n}
+      id={index}
+      number={item.n}
+      src={item.src}
+      handleClick={shuffle}/>)
       }
     </div>
   )
