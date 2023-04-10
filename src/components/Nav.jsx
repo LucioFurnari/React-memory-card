c70a9ede2c12a0462841c1e111f67a0ee099d6cd
LucioFurnari <luciofurnari99@gmail.com>
2023-04-10 12:13:19 -0300

    Separate the nav in divs

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,13 @@ export default function Nav(props) {
   const { score, bestScore } = props;
   return (
     <nav>
+      <div>
       <h1>Memory Card</h1>
       <p>Get points by clicking on an image but don't click on any more than once!</p>
+      </div>
       <div className="score-table">
-        <span>Score: {score}</span>
-        <span>Best score: {bestScore}</span>
+        <p>Score: {score}</p>
+        <p>Best score: {bestScore}</p>
       </div>
     </nav>
   )
