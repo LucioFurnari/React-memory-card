480e2a613da83ae650ee2f4e81c8fdb315ff084b
LucioFurnari <luciofurnari99@gmail.com>
2023-04-10 12:14:11 -0300

    Add img tag with src

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 export default function Card(props) {
-  const {number, handleClick, id} = props
+  const {number, handleClick, id, src} = props
   return (
     <div className="card" onClick={handleClick} id={id}>
       <p>{number}</p>
+      <img src={src}></img>
     </div>
   )
 }
